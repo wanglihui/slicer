531dbea4127c6d76cc99ce8946f1f8d3e744e09a
wanglihui <lihui@guanghe.com>
2021-12-14 17:24:31 +0800

    modify variable name

--- fns/maps/template.go
+++ fns/maps/template.go
@@ -15,62 +15,62 @@ type MapTemplate struct{}
 
 func (tpl *MapTemplate) GetContent(out io.Writer, data core.GenerateData) error {
 	t, err := template.New("").Parse(`
-type {{.Type}}MapFn func(pkg {{.Type}}, idx int) interface{}
-type {{.Type}}Map{{.Type}}Fn func(pkg {{.Type}}, idx int) {{.Type}}
-type {{.Type}}MapStringFn func(pkg {{.Type}}, idx int) string
-type {{.Type}}MapIntFn func(pkg {{.Type}}, idx int) int
-type {{.Type}}MapInt64Fn func(pkg {{.Type}}, idx int) int64
-type {{.Type}}MapFloat64Fn func(pkg {{.Type}}, idx int) float64
+type {{.Type}}MapFn func(item {{.Type}}, idx int) interface{}
+type {{.Type}}Map{{.Type}}Fn func(item {{.Type}}, idx int) {{.Type}}
+type {{.Type}}MapStringFn func(item {{.Type}}, idx int) string
+type {{.Type}}MapIntFn func(item {{.Type}}, idx int) int
+type {{.Type}}MapInt64Fn func(item {{.Type}}, idx int) int64
+type {{.Type}}MapFloat64Fn func(item {{.Type}}, idx int) float64
 type {{.Type}}MapReturnedInterface []interface{}
 
-func (packages {{.Type}}Slice) Map(fn {{.Type}}MapFn) {{.Type}}MapReturnedInterface {
+func (items {{.Type}}Slice) Map(fn {{.Type}}MapFn) {{.Type}}MapReturnedInterface {
 	arr := make([]interface{}, 0)
-	for idx, pkg := range packages {
-		arr = append(arr, fn(pkg, idx))
+	for idx, item := range items {
+		arr = append(arr, fn(item, idx))
 	}
 	return arr
 }
 
-func (packages {{.Type}}Slice) MapString(fn {{.Type}}MapStringFn) []string {
+func (items {{.Type}}Slice) MapString(fn {{.Type}}MapStringFn) []string {
 	rets := make([]string, 0)
-	for idx, pkg := range packages {
-		s := fn(pkg, idx)
+	for idx, item := range items {
+		s := fn(item, idx)
 		rets = append(rets, s)
 	}
 	return rets
 }
 
-func (packages {{.Type}}Slice) MapInt(fn {{.Type}}MapIntFn) []int {
+func (items {{.Type}}Slice) MapInt(fn {{.Type}}MapIntFn) []int {
 	rets := make([]int, 0)
-	for idx, pkg := range packages {
-		s := fn(pkg, idx)
+	for idx, item := range items {
+		s := fn(item, idx)
 		rets = append(rets, s)
 	}
 	return rets
 }
 
-func (packages {{.Type}}Slice) MapInt64(fn {{.Type}}MapInt64Fn) []int64 {
+func (items {{.Type}}Slice) MapInt64(fn {{.Type}}MapInt64Fn) []int64 {
 	rets := make([]int64, 0)
-	for idx, pkg := range packages {
-		s := fn(pkg, idx)
+	for idx, item := range items {
+		s := fn(item, idx)
 		rets = append(rets, s)
 	}
 	return rets
 }
 
-func (packages {{.Type}}Slice) MapFloat64(fn {{.Type}}MapFloat64Fn) []float64 {
+func (items {{.Type}}Slice) MapFloat64(fn {{.Type}}MapFloat64Fn) []float64 {
 	rets := make([]float64, 0)
-	for idx, pkg := range packages {
-		s := fn(pkg, idx)
+	for idx, item := range items {
+		s := fn(item, idx)
 		rets = append(rets, s)
 	}
 	return rets
 }
 
-func (packages {{.Type}}Slice) Map{{.Type}}(fn {{.Type}}Map{{.Type}}Fn) []{{.Type}} {
+func (items {{.Type}}Slice) Map{{.Type}}(fn {{.Type}}Map{{.Type}}Fn) []{{.Type}} {
 	rets := make([]{{.Type}}, 0)
-	for idx, pkg := range packages {
-		s := fn(pkg, idx)
+	for idx, item := range items {
+		s := fn(item, idx)
 		rets = append(rets, s)
 	}
 	return rets
